bf6050dca656b1c7a98d5a5202e14f5a79a1244f
EC2 Default User <ec2-user@ip-172-31-38-78.ap-northeast-1.compute.internal>
2019-09-10 12:04:52 +0000

    edited init/db.py

--- init/db.py
+++ init/db.py
@@ -8,7 +8,15 @@ if users.count():
     print('found')
 else:
     print('not found')
-for user in users:
-    print(user)
+cnt=0
+thre=2000
+upau=0
+for i,user in enumerate(users):
+    print(i, user)
+    if(user['count'] > thre):
+        cnt+=1;
+    if(user['authority'] >= 1):
+        upau+=1
+print(f"{thre}回以上コマンドを実行した人は{cnt}人です。レベル1以上の権限保持者は{upau}人です。")
 
 print('max count', collection.find_one(sort=[("count", -1)]))
